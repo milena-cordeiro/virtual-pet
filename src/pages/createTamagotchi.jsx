c0d901cd90ef7aeff9f4f7995432b412677a234a
Milena Cordeiro <mcs9823@gmail.com>
2023-07-31 15:33:11 -0300

    Configurações iniciais e criando a página de criar um pet virtual

---
+++ src/pages/createTamagotchi.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function CreateTamagotchi() {
+  const [tamagotchi, setTamagotchi] = useState({
+    health: 100,
+    happiness: 100,
+    hunger: 0
+  });
+
+  const [name, setName] = useState('');
+  const [type, setType] = useState('cat');
+
+  const createNewTamagotchi = () => {
+    setTamagotchi({
+      name,
+      type,
+      health: tamagotchi.health,
+      happiness: tamagotchi.happiness,
+      hunger: tamagotchi.hunger
+    });
+  }
+
+  return (
+    <div>
+      <h1>Bem vindo!</h1>
+      <h2>Crie seu pet virtual</h2>
+      <form action="">
+        <label htmlFor="tamagotchi-name">Nome:
+        <input type="text" value={name} id="tamagotchi-name" onChange={(e) => setName(e.target.value)}/>
+        </label>
+        <label htmlFor="tamagotchi-type">Qual tipo:
+        <select value={type} id="tamagotchi-type" onChange={(e) => setType(e.target.value)}>
+          <option value="cat">Gato</option>
+          <option value="dog">Cachorro</option>
+          <option value="hamster">Hamster</option>
+          <option value="turtle">Tartaruga</option>
+          <option value="fish">Peixe</option>
+          <option value="bird">Passáro</option>
+          <option value="horse">Cavalo</option>
+          <option value="unicorn">Unicornio</option>
+          <option value="penguin">Pinguim</option>
+          <option value="rabbit">Coelho</option>
+        </select>
+        </label>
+      </form>
+      <br />
+      <button type="button" onClick={createNewTamagotchi}>Start</button>
+    </div>
+  )
+
+};
+
+export default CreateTamagotchi;
